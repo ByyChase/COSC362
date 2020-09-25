01f4c45938401c175c8a2ae0fb4e76df3d5cf3f5
ByyChase <zwtw@iup.edu>
2020-09-24 19:59:57 -0700

    Rocket Launch Ascii Art

---
+++ EpicArt.sh
@@ -0,0 +1,123 @@
+
+#1/src/bash
+
+clear 
+
+echo "   _____ "
+echo "  |____ |" 
+echo "     / / " 
+echo "     \ \ "
+echo " .___/ / "
+echo " \____/  "
+
+sleep 2 
+
+clear 
+
+
+echo "  _____  "
+echo " /___  \ "
+echo "    / /  "
+echo "   / /   "
+echo " _/ /___ "
+echo " \_____/ "
+
+sleep 2
+
+clear
+
+
+echo " /  | "
+echo "  | | "
+echo "  | | "
+echo " _| |_"
+echo " \___/"
+
+sleep 2 
+
+clear
+
+
+echo "   _      _   __  _            __   __  "
+echo "  | |    (_) / _|| |          / _| / _| "
+echo "  | |     _ | |_ | |_   ___  | |_ | |_  "
+echo "  | |    | ||  _|| __| / _ \ |  _||  _| "
+echo "  | |____| || |  | |_ | (_) || |  | |  "
+echo "  \_____/|_||_|   \__| \___/ |_|  |_| "
+
+
+sleep 2 
+
+clear 
+
+
+echo "        ! "
+echo "        ! "
+echo "        ^ "
+echo "       / \ " 
+echo "      /___\ "
+echo "     |=   =| " 
+echo "     |     | " 
+echo "     |     | " 
+echo "     |     | " 
+echo "     |     | " 
+echo "     |     | " 
+echo "     |     | " 
+echo "     |     | " 
+echo "     |     | " 
+echo "     |     | "
+echo "    /|##!##|\ "
+echo "   / |##!##| \ " 
+echo "  /  |##!##|  \ " 
+echo " |  / ^ | ^ \  | " 
+echo " | /  ( | )  \ | " 
+echo " |/   ( | )   \|" 
+echo "     ((   )) " 
+echo "    ((  :  )) " 
+echo "    ((  :  )) " 
+echo "     ((   )) " 
+echo "      (( )) " 
+echo "       ( ) " 
+echo "        . " 
+echo "        . " 
+echo "        . " 
+sleep 1
+echo " "
+echo " " 
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
+sleep 1
+echo " "
+echo " "
+echo " "
